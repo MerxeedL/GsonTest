1ece7aae295cd9ca0865081d3388606b70203e02
Evgeniy.Lychkovsky <cdsa1995@mail.ru>
2024-02-05 09:55:06 +0300

    Ended task

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,105 @@
 import com.google.gson.Gson;
 
-import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.Locale.filter;
-import static java.util.stream.Collectors.toList;
 
 public class Main {
-    public static <T, E> void main(String[] args) throws IOException {
 
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        try {
+            FileWriter writer = new FileWriter("src\\FileForWrite.txt", false);
 
-        String fileName = "src//tickets.json";
+            StringBuilder sb = new StringBuilder();
+            double median;
 
-        Path path = Paths.get(fileName);
-        Scanner sc = new Scanner(path);
+            String fileName = "src//tickets.json";
 
-        while (sc.hasNext()) {
-            sb.append(sc.next());
+            Path path = Paths.get(fileName);
+            Scanner sc = new Scanner(path);
+
+            while (sc.hasNext()) {
+                sb.append(sc.next());
+            }
+
+            Gson test = new Gson();
+            Tickets ticket = test.fromJson(sb.toString(), Tickets.class);
+
+            List<Ticket> listTickets = ticket.getTickets();
+
+            writer.write("Carrier: TK\n");
+            writer.write(listTickets.stream()
+                                       .filter(c -> c.getCarrier().equals("TK"))
+                                       .filter(o -> o.getOrigin().equals("VVO"))
+                                       .filter(d -> d.getDestination().equals("TLV"))
+                                       .map(Ticket::getDifferentTime)
+                                       .min(Comparator.naturalOrder())
+                                       .get() + " minutes\n");
+            writer.write("----------------\n");
+
+            writer.write("Carrier: S7\n");
+            writer.write(listTickets.stream()
+                                       .filter(c -> c.getCarrier().equals("S7"))
+                                       .filter(o -> o.getOrigin().equals("VVO"))
+                                       .filter(d -> d.getDestination().equals("TLV"))
+                                       .map(Ticket::getDifferentTime)
+                                       .min(Comparator.naturalOrder())
+                                       .get() + " minutes\n");
+            writer.write("----------------\n");
+
+            writer.write("Carrier: SU\n");
+            writer.write(listTickets.stream()
+                                       .filter(c -> c.getCarrier().equals("SU"))
+                                       .filter(o -> o.getOrigin().equals("VVO"))
+                                       .filter(d -> d.getDestination().equals("TLV"))
+                                       .map(Ticket::getDifferentTime)
+                                       .min(Comparator.naturalOrder())
+                                       .get() + " minutes\n");
+            writer.write("----------------\n");
+
+            writer.write("Carrier: BA\n");
+            writer.write(listTickets.stream()
+                                       .filter(c -> c.getCarrier().equals("BA"))
+                                       .filter(o -> o.getOrigin().equals("VVO"))
+                                       .filter(d -> d.getDestination().equals("TLV"))
+                                       .map(Ticket::getDifferentTime)
+                                       .min(Comparator.naturalOrder())
+                                       .get() + " minutes\n");
+            writer.write("----------------\n");
+
+            List<Integer> listPrices = listTickets.stream()
+                    .filter(od -> od.getOrigin().equals("VVO") && od.getDestination().equals("TLV"))
+                    .mapToInt(Ticket::getPrice)
+                    .sorted()
+                    .boxed()
+                    .collect(Collectors.toList());
+
+            OptionalDouble avg = listPrices.stream()
+                    .mapToDouble(a -> a)
+                    .average();
+
+            writer.write(avg.getAsDouble() + " - average price\n");
+            double average = avg.getAsDouble();
+
+            int size = listPrices.size();
+
+            if (size % 2 == 0) {
+                median = (listPrices.get(size / 2 - 1) + listPrices.get(size / 2)) / 2.0;
+            } else {
+                median = listPrices.get(size / 2);
+            }
+            writer.write(median + " - median price\n");
+            double result = average - median;
+
+            writer.write(result + " - difference between average price and median\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Oshibka");
         }
 
-        Gson test = new Gson();
-        Tickets ticket = test.fromJson(sb.toString(), Tickets.class);
-
-        List<Ticket> listTickets = ticket.getTickets();
-
-
-        System.out.println("Carrier: TK");
-        System.out.println(listTickets.stream()
-                                   .filter(c -> c.getCarrier().equals("TK"))
-                                   .filter(o -> o.getOrigin().equals("VVO"))
-                                   .filter(d -> d.getDestination().equals("TLV"))
-                                   .map(Ticket::getDifferentTime)
-                                   .min(Comparator.naturalOrder())
-                                   .get() + " minutes");
-        System.out.println("----------------");
-
-        System.out.println("Carrier: S7");
-        System.out.println(listTickets.stream()
-                                   .filter(c -> c.getCarrier().equals("S7"))
-                                   .filter(o -> o.getOrigin().equals("VVO"))
-                                   .filter(d -> d.getDestination().equals("TLV"))
-                                   .map(Ticket::getDifferentTime)
-                                   .min(Comparator.naturalOrder())
-                                   .get() + " minutes");
-        System.out.println("----------------");
-
-        System.out.println("Carrier: SU");
-        System.out.println(listTickets.stream()
-                                   .filter(c -> c.getCarrier().equals("SU"))
-                                   .filter(o -> o.getOrigin().equals("VVO"))
-                                   .filter(d -> d.getDestination().equals("TLV"))
-                                   .map(Ticket::getDifferentTime)
-                                   .min(Comparator.naturalOrder())
-                                   .get() + " minutes");
-        System.out.println("----------------");
-
-        System.out.println("Carrier: BA");
-        System.out.println(listTickets.stream()
-                                   .filter(c -> c.getCarrier().equals("BA"))
-                                   .filter(o -> o.getOrigin().equals("VVO"))
-                                   .filter(d -> d.getDestination().equals("TLV"))
-                                   .map(Ticket::getDifferentTime)
-                                   .min(Comparator.naturalOrder())
-                                   .get() + " minutes");
-        System.out.println("----------------");
-
-        System.out.println(listTickets.stream()
-                                   .filter(o -> o.getOrigin().equals("VVO"))
-                                   .filter(d -> d.getDestination().equals("TLV"))
-                                   .mapToInt(Ticket::getPrice).average()
-                                   .getAsDouble() + " - average price");
-
-        List<Integer> listPrices = listTickets.stream()
-                .filter(o -> o.getOrigin().equals("VVO"))
-                .filter(d -> d.getDestination().equals("TLV"))
-                .mapToInt(Ticket::getPrice)
-                .sorted()
-                .collect(Collectors.toList());
-
-        System.out.println(listPrices);
 
     }
 }
